9dbef4e748c2847ee7d30cdef6296d6fb89b0136
sriv <srikanth.ddit@gmail.com>
2015-02-18 17:25:03 +0530

    fixed install script url

--- install_latest_gauge.sh
+++ install_latest_gauge.sh
@@ -1,22 +1,23 @@
-sudo yum install unzip
-
 GAUGE_LATEST=`curl -w "%{url_effective}\n" -L -s -S https://github.com/getgauge/gauge/releases/latest -o /dev/null`
 
 GAUGE_LATEST_VERSION=`echo $GAUGE_LATEST | sed 's/.*\/v//'`
+
 BIT=`uname -m`
 
 if [ "$BIT"=="x86_64" ];
 then 
-	DOWNLOAD="gauge-$GAUGE_LATEST_VERSION-linux.x86_64.zip"
+	GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_VERSION-linux.x86_64.zip"
 else
-	DOWNLOAD="gauge-$GAUGE_LATEST_VERSION-linux.x86.zip"
+	GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_VERSION-linux.x86.zip"
 fi
 
-wget "$GAUGE_LATEST/$DOWNLOAD"
+GAUGE_DOWNLOAD_URL="https://github.com/getgauge/gauge/releases/download/v$GAUGE_LATEST_VERSION/$GAUGE_FILE_NAME"
+
+wget GAUGE_DOWNLOAD_URL
 
 OUTPUT_DIR="./gauge_$GAUGE_LATEST_VERSION"
 
-unzip $DOWNLOAD -d $OUTPUT_DIR
+unzip $GAUGE_FILE_NAME -d $OUTPUT_DIR
 
 cd $OUTPUT_DIR
 
