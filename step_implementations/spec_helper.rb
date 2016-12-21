c8f4257ce3ccce26b43001ff6bacd14e9121fab7
Apollo Catlin <apollo@threatstack.com>
2016-12-20 23:23:08 -0700

    unfreeze strings

--- step_implementations/spec_helper.rb
+++ step_implementations/spec_helper.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 require_relative './pages/base_page.rb'
 Dir[File.join(File.dirname(__FILE__), './pages/*.rb')].each { |file| require file }
 
--- step_implementations/user_spec.rb
+++ step_implementations/user_spec.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 require_relative 'spec_helper.rb'
 
 step 'On signup page' do
